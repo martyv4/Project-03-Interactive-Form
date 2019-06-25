2b4f05f46a38c5859069147ed5571288494a70d5
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-06-25 15:49:16 -0400

    work on app.js file and html script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$("#name").focus();
+
+/* $("#other-title").hide();
+
+$("#design").find('option').first().hide();
+$("#color option:first").before('<option selected="selected">Please select a T-shirt theme</option>');
+$("#color").find('option:not(:eq(0))').hide(); */
