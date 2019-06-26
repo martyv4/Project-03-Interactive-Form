c0160f87a178f60fdf8b344746efb65463050414
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-06-26 10:42:33 -0400

    T-Shirt Section
    T-Shirt Section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
+//pound sign something is looking for the id something
+//<input type="text" id="name" name="user_name">
 $("#name").focus();
 
-/* $("#other-title").hide();
+//<input type="text" id="other-title" name="job_role_other" placeholder="Your Job Role">
+$("#other-title").hide();
 
+//T-SHIRT section starts here
+//Hide the  “Select Theme”  `option` element in the “Design” menu
+//<select id="design" name="user_design">
 $("#design").find('option').first().hide();
-$("#color option:first").before('<option selected="selected">Please select a T-shirt theme</option>');
-$("#color").find('option:not(:eq(0))').hide(); */
+
+//Update the “Color” field to read “Please select a T-shirt theme”.
+//<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+$("#color option:first").before('<option selected="selected" value="none">Please select a T-shirt theme</option>');
+
+//Hide the colors in the “Color” drop down menu.
+//<select id="color">
+$("#color").find('option:not(:eq(0))').hide();
+
+//event listener if show hide update color
+$('#design').change(function() {
+
+    let choice = $('#design option:selected').attr("value");
+    if (choice == 'js puns') {
+        $('#color option[value="none"]').remove();
+        $('#color option:contains("JS Puns")').show();
+        $('#color option:contains("JS shirt only")').first().attr('selected', false);
+        $('#color option:contains("JS shirt only")').hide();
+        $('#color option:contains("JS Puns")').first().attr('selected', 'selected');
+    }
+    else if (choice == 'heart js') {
+        $('#color option[value="none"]').remove();
+        $('#color option:contains("JS Puns")').hide();
+        $('#color option:contains("JS Puns")').first().attr('selected', false);
+        $('#color option:contains("JS shirt only")').show();
+        $('#color option:contains("JS shirt only")').first().attr('selected', 'selected');
+    }
+    else {
+        $("#color option:first").before('<option selected="selected" value="none">Please select a T-shirt theme</option>');
+    }
+});
+//Creating an element to display the total activity cost
+    var activityCostDiv = document.createElement("div");  // Create with DOM https://www.w3schools.com/jquery/jquery_dom_add.asp
+    $(".activities").append(activityCostDiv);      // Append the new element
+    var activityCostAmt = 0.0;
